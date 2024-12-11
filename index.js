23eb884e989287bfa5e9fe135db244f135040586
TheoUng <118573824+TheoUng@users.noreply.github.com>
2024-12-10 21:34:57 -0800

    Résolutiof conflits  index.js

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-const express = require('express');
-<<<<<<< HEAD
-const path = require("path");
-const app = express();
-const port = 3000; // You can choose any port that is free on your system
-
-// Correct the path to the relative path assuming weatherData.js is in the same directory
-const weatherData = require("./weatherData");
-
-app.get('/', (req, res) => {
-  res.send("Weather app running");
-});
-
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send('Address is required');
-    }
-    weatherData(req.query.address, (error, result) => {
-        if (error) {
-            return res.send('Error: ' + result);
-        }
-        res.send(result);
-    });
-});
-
-app.get("*", (req, res) => {
-    res.send("This route doesn't exists");
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-=======
 const cors = require('cors');
 const axios = require('axios');
 
@@ -81,5 +49,4 @@ app.get('/weather', async (req, res) => {
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
->>>>>>> e8c1ea4 (Premier commit)
 });
